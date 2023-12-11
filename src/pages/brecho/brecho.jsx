5396666a2c90689b6949547d093079642a00db6c
Vitor Augusto <108837467+gwtovitor@users.noreply.github.com>
2023-12-11 08:29:28 -0300

    up

--- src/pages/brecho/brecho.jsx
+++ src/pages/brecho/brecho.jsx
@@ -1,26 +1,48 @@
-import styles from './brecho.module.scss'
-import { roupas } from './brecho.mock'
-export default function Brecho() {
-    const roupasArray = Object.values(roupas);
+import { useState, useEffect } from 'react';
+import styles from './brecho.module.scss';
+import { roupas } from './brecho.mock';
 
-    return (
-        <div className={styles.wrapper}>
-            <div>
-                <button></button>
-                <button></button>
-                <button></button>
-            </div>
-            <div className={styles.wrapperGrid}>
-                {roupasArray.map((roupa,index) => (
-                    <div key={index} className={styles.wrapperItem}>
-                        <img width={130} src={`../../../public/Roupas/${roupa.id}/1.jpg`}></img>
-                        <h2>{roupa.nome}</h2>
-                        <span>{roupa.descricao}</span>
-                        <span>{roupa.valor}</span>
-                        
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+export default function Brecho() {
+  const [roupasExibidas, setRoupasExibidas] = useState([]);
+
+  useEffect(() => {
+    setRoupasExibidas(Object.values(roupas).flatMap(Object.values));
+  }, []);
+
+  const handleExibirTodos = () => {
+    setRoupasExibidas(Object.values(roupas).flatMap(Object.values));
+  };
+
+  const handleExibirInferiores = () => {
+    setRoupasExibidas(Object.values(roupas.Inferiores));
+  };
+
+  const handleExibirSuperiores = () => {
+    setRoupasExibidas(Object.values(roupas.Superiores));
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <button onClick={handleExibirTodos}>Exibir Todos</button>
+        <button onClick={handleExibirInferiores}>Apenas Inferiores</button>
+        <button onClick={handleExibirSuperiores}>Apenas Superiores</button>
+      </div>
+      <div className={styles.wrapperGrid}>
+        {roupasExibidas.map((roupa, index) => (
+          <div key={index} className={styles.wrapperItem}>
+            <img
+              className={styles.imagem}
+              width={130}
+              src={`../../../public/Roupas/${roupa.id}/1.jpg`}
+              alt={roupa.nome}
+            />
+            <span className={styles.title}>{roupa.nome}</span>
+            <span className={styles.descricao}>{roupa.descricao}</span>
+            <span className={styles.valor}>{roupa.valor}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/pages/brecho/brecho.mock.jsx
+++ src/pages/brecho/brecho.mock.jsx
@@ -1,16 +1,34 @@
 export const roupas = {
-    'Superiores':{
-        'id': 1,
-        'nome': 'Top canelado',
-        'descricao': 'Manga longa',
-        'valor': 'R$ 25,00',
-        'fotos': 1,
+    'Superiores': {
+        0: {
+            'id': 1,
+            'nome': 'Top canelado',
+            'descricao': 'Manga longa',
+            'valor': 'R$ 25,00',
+            'fotos': 1,
+        },
+        1: {
+            'id': 2,
+            'nome': 'Top canelado',
+            'descricao': 'Manga longa',
+            'valor': 'R$ 25,00',
+            'fotos': 1,
+        }
     },
-    'Inferiores':{
-        'id': 2,
-        'nome': 'Mini saia rodada',
-        'descricao': 'Tamanho PP',
-        'valor': 'R$ 40,00',
-        'fotos': 2,
+    'Inferiores': {
+        0: {
+            'id': 2,
+            'nome': 'Mini saia rodada',
+            'descricao': 'Tamanho PP',
+            'valor': 'R$ 40,00',
+            'fotos': 2,
+        },
+        1: {
+            'id': 2,
+            'nome': 'Mini saia rodada',
+            'descricao': 'Tamanho PP',
+            'valor': 'R$ 40,00',
+            'fotos': 2,
+        },
     },
 }
